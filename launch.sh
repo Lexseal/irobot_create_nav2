159263d8e8f0baac50e763bb401b7a0e3140b358
Xinsong Lin <zhongjiezhex12@me.com>
2024-05-30 15:36:38 -0700

    working params

--- launch.sh
+++ launch.sh
@@ -20,8 +20,7 @@ tmux split-window -v -t mysession
 
 # Send commands to each pane
 tmux send-keys -t mysession:0.0 'ros2 run zlac8015d_ros driver /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_B0019X5N-if00-port0' C-m
-tmux send-keys -t mysession:0.1 'ros2 launch irobot_create_nav2 
-navigation2.launch.py serial_port:=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0' C-m
+tmux send-keys -t mysession:0.1 'ros2 launch irobot_create_nav2 navigation2.launch.py serial_port:=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0' C-m
 tmux send-keys -t mysession:0.2 'ros2 launch realsense2_camera rs_launch.py rgb_camera.profile:=960x540x6 depth_module.profile:=640x480x30 enable_sync:=True align_depth.enable:=True' C-m
 tmux send-keys -t mysession:0.3 '' C-m
 
